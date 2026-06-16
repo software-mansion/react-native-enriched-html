12c8c1105f80d5242eb111d4bd97c6d6c3f82e30
Igor Furgala <exploif@icloud.com>
2026-06-16 17:32:16 +0200

    chore: deprecate

--- src/deprecate.ts
+++ src/deprecate.ts
@@ -1,7 +1,5 @@
 export const warnDeprecated = () => {
-  if (__DEV__) {
-    console.warn(`react-native-enriched has been renamed to react-native-enriched-html.
+  console.warn(`react-native-enriched has been renamed to react-native-enriched-html.
 This package will not receive any further updates or bug fixes. Migrate without any breaking changes using: "npm install react-native-enriched-html".
 Official announcement: https://github.com/software-mansion/react-native-enriched-html/releases/tag/v1.0.0`);
-  }
 };
