4749fc787a0e9922ef90c0aff249cc51b429fb3b
Krystian Sienkiewicz <krystian.sienkiewicz@swmansion.com>
2026-07-09 15:21:15 +0200

    fix: list markers position in text alignment

--- src/web/normalization/prepareHtmlForWeb.ts
+++ src/web/normalization/prepareHtmlForWeb.ts
@@ -1,5 +1,60 @@
-export function prepareHtmlForWeb(html: string) {
-  return checkboxHtmlToWeb(html);
+export function prepareHtmlForWeb(html: string): string {
+  if (typeof DOMParser === 'undefined') return html;
+
+  const parser = new DOMParser();
+  const doc = parser.parseFromString(html, 'text/html');
+
+  wrapBareLiContentInParagraph(doc);
+  checkboxHtmlToWeb(doc);
+
+  return doc.body.innerHTML;
+}
+
+/*
+ * Native list format:
+ *   <ul>
+ *     <li>foo</li>
+ *     <li></li>
+ *   </ul>
+ *
+ * Web-native, with <p> wrappers to display the content correctly:
+ *   <ul>
+ *     <li>
+ *       <p>foo</p>
+ *     </li>
+ *     <li>
+ *       <p></p>
+ *     </li>
+ *   </ul>
+ */
+function wrapBareLiContentInParagraph(doc: Document) {
+  // Target only standard lists (ignore checkbox lists, as they get wrapped in <label> later)
+  const listItems = doc.querySelectorAll(
+    'ul:not([data-type="checkbox"]) > li, ol > li'
+  );
+
+  listItems.forEach((li) => {
+    if (li.firstElementChild?.tagName.toUpperCase() === 'P') return;
+
+    const nodesToWrap: Node[] = [];
+    const childNodes = Array.from(li.childNodes);
+
+    for (const node of childNodes) {
+      if (
+        node.nodeType === Node.ELEMENT_NODE &&
+        ['UL', 'OL'].includes((node as Element).tagName.toUpperCase())
+      ) {
+        break;
+      }
+      nodesToWrap.push(node);
+    }
+
+    const p = doc.createElement('p');
+
+    li.insertBefore(p, childNodes[0] || null);
+
+    nodesToWrap.forEach((node) => p.appendChild(node));
+  });
 }
 
 /*
@@ -21,12 +76,7 @@ export function prepareHtmlForWeb(html: string) {
  *     </li>
  *   </ul>
  */
-function checkboxHtmlToWeb(html: string): string {
-  if (typeof DOMParser === 'undefined') return html;
-
-  const parser = new DOMParser();
-  const doc = parser.parseFromString(html, 'text/html');
-
+function checkboxHtmlToWeb(doc: Document) {
   doc.querySelectorAll('ul[data-type="checkbox"]').forEach((ul) => {
     ul.querySelectorAll('li').forEach((li) => {
       const checked = li.hasAttribute('checked');
@@ -38,6 +88,4 @@ function checkboxHtmlToWeb(html: string): string {
         `<label>${labelContent}</label>`;
     });
   });
-
-  return doc.body.innerHTML;
 }
