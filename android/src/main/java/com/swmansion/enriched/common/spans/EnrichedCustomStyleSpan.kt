d940c5e453fa857157f5e5de7e87c9f579a8ff49
Kacper Żółkiewski <kacper.zolkiewski@swmansion.com>
2026-07-29 15:14:45 +0200

    fix: handling colors opacity

--- android/src/main/java/com/swmansion/enriched/common/spans/EnrichedCustomStyleSpan.kt
+++ android/src/main/java/com/swmansion/enriched/common/spans/EnrichedCustomStyleSpan.kt
@@ -1,5 +1,6 @@
 package com.swmansion.enriched.common.spans
 
+import android.graphics.Color
 import android.text.TextPaint
 import android.text.style.CharacterStyle
 import com.swmansion.enriched.common.spans.interfaces.EnrichedInlineSpan
@@ -14,7 +15,16 @@ open class EnrichedCustomStyleSpan(
   fun getBackgroundColor(): Int? = backgroundColor
 
   override fun updateDrawState(textPaint: TextPaint) {
-    foregroundColor?.let { textPaint.color = it }
-    backgroundColor?.let { textPaint.bgColor = it }
+    foregroundColor?.let { textPaint.color = withOpacity(it, 80) }
+    backgroundColor?.let { textPaint.bgColor = withOpacity(it, 80) }
+  }
+
+  private fun withOpacity(
+    color: Int,
+    alpha: Int,
+  ): Int {
+    if (Color.alpha(color) != 255) return color
+    val a = alpha.coerceIn(0, 255)
+    return (color and 0x00FFFFFF) or (a shl 24)
   }
 }
